3e51a8d958149bbe34530804493c6f1f8a4ba1be
filipe-machado <ax-filipe@hotmail.com>
2019-08-31 05:34:14 -0500

    Updates

---
+++ precache-manifest.6f5241fcc2a1acff74cac0b856a549ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58e57eae9331ca20290c29a64aa4ca90",
+    "url": "/template/index.html"
+  },
+  {
+    "revision": "24369ed4cc98fa2a45cd",
+    "url": "/template/static/css/main.0400036b.chunk.css"
+  },
+  {
+    "revision": "7f75b33345a54b61f032",
+    "url": "/template/static/js/2.9c574eec.chunk.js"
+  },
+  {
+    "revision": "24369ed4cc98fa2a45cd",
+    "url": "/template/static/js/main.5a4c524e.chunk.js"
+  },
+  {
+    "revision": "e927ed3b744e96d7841e",
+    "url": "/template/static/js/runtime~main.0a286741.js"
+  },
+  {
+    "revision": "35503679e9d96f9e86396761d6e5318a",
+    "url": "/template/static/media/chart.35503679.svg"
+  },
+  {
+    "revision": "e0b8ba330049baaf8544d9d6571194a1",
+    "url": "/template/static/media/homem.e0b8ba33.svg"
+  },
+  {
+    "revision": "abf01a0a01d7c9c37e3b1a594fcacf11",
+    "url": "/template/static/media/menu.abf01a0a.svg"
+  },
+  {
+    "revision": "566855f28852e622581a60d2cbe59aaa",
+    "url": "/template/static/media/mulher.566855f2.svg"
+  },
+  {
+    "revision": "46545164c506d64d7db996cb16cc7ec3",
+    "url": "/template/static/media/triangle.46545164.svg"
+  }
+]);
